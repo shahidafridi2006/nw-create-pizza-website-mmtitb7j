080146dbf06233fca1c33b154e60fa95cce0d956
shahidafridi2006 <shahidafridi424244@gmail.com>
2026-03-16 23:35:26 +0500

    feat: add src/lib/supabase.ts [NovaWarm]

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from '@/types/supabase';
 
-const supabaseUrl = "__SUPABASE_URL__";
-const supabaseAnonKey = "__SUPABASE_ANON_KEY__";
+const supabaseUrl = "https://sfasknpxifudlaxhyyuj.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmYXNrbnB4aWZ1ZGxheGh5eXVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2ODQ3NjgsImV4cCI6MjA4OTI2MDc2OH0.GJ87O9bVJy--i8LWactKYCe4n5Kdx85jIzu61DXslV0";
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
